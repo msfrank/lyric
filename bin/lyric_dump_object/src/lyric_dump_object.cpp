c2f7509457383893a4c6d01aba596031c640165e
Michael Frank <msfrank@syntaxjockey.com>
2026-01-27 11:37:08 -0800

    use Command class for command line processing

--- bin/lyric_dump_object/src/lyric_dump_object.cpp
+++ bin/lyric_dump_object/src/lyric_dump_object.cpp
@@ -5,7 +5,7 @@
 
 #include <lyric_object/internal/object_reader.h>
 #include <lyric_object/lyric_object.h>
-#include <tempo_command/command_help.h>
+#include <tempo_command/command.h>
 #include <tempo_config/base_conversions.h>
 #include <tempo_utils/file_reader.h>
 #include <tempo_utils/unicode.h>
@@ -18,66 +18,20 @@ dump_lyric_object(int argc, const char *argv[])
     tempo_config::IntegerParser quietParser(0);
     tempo_config::BooleanParser silentParser(false);
 
-    std::vector<tempo_command::Default> cmdDefaults = {
-        {"verbose", "Display verbose output (specify twice for even more verbose output)"},
-        {"quiet", "Display warnings and errors only (specify twice for errors only)"},
-        {"silent", "Suppress all output"},
-        {"objectPath", "path to the object to dump", "PATH"},
-    };
+    tempo_command::Command command("lyric-dump-object");
 
-    const std::vector<tempo_command::Grouping> cmdGroupings = {
-        {"verbose", {"-v"}, tempo_command::GroupingType::NO_ARGUMENT},
-        {"quiet", {"-q"}, tempo_command::GroupingType::NO_ARGUMENT},
-        {"silent", {"-s", "--silent"}, tempo_command::GroupingType::NO_ARGUMENT},
-        {"help", {"-h", "--help"}, tempo_command::GroupingType::HELP_FLAG},
-        {"version", {"--version"}, tempo_command::GroupingType::VERSION_FLAG},
-    };
+    command.addArgument("objectPath", "PATH", tempo_command::MappingType::ONE_INSTANCE,
+        "Path to the object to dump");
+    command.addFlag("verbose", {"-v"}, tempo_command::MappingType::COUNT_INSTANCES,
+        "Display verbose output (specify twice for even more verbose output)");
+    command.addFlag("quiet", {"-q"}, tempo_command::MappingType::COUNT_INSTANCES,
+        "Display warnings and errors only (specify twice for errors only)");
+    command.addFlag("silent", {"-s", "--silent"}, tempo_command::MappingType::TRUE_IF_INSTANCE,
+        "Suppress all output");
+    command.addHelpOption("help", {"-h", "--help"},
+        "Dump a lyric object into a human-readable format");
 
-    const std::vector<tempo_command::Mapping> optMappings = {
-        {tempo_command::MappingType::COUNT_INSTANCES, "verbose"},
-        {tempo_command::MappingType::COUNT_INSTANCES, "quiet"},
-        {tempo_command::MappingType::TRUE_IF_INSTANCE, "silent"},
-    };
-
-    std::vector<tempo_command::Mapping> argMappings = {
-        {tempo_command::MappingType::ONE_INSTANCE, "objectPath"},
-    };
-
-    // parse argv array into a vector of tokens
-    auto tokenizeResult = tempo_command::tokenize_argv(argc - 1, &argv[1]);
-    if (tokenizeResult.isStatus())
-        tempo_command::display_status_and_exit(tokenizeResult.getStatus());
-    auto tokens = tokenizeResult.getResult();
-
-    std::string subcommand;
-    tempo_command::OptionsHash cmdOptions;
-    tempo_command::ArgumentVector cmdArguments;
-
-    // parse global options and get the subcommand
-    auto status = tempo_command::parse_completely(tokens, cmdGroupings, cmdOptions, cmdArguments);
-    if (status.notOk()) {
-        tempo_command::CommandStatus commandStatus;
-        if (!status.convertTo(commandStatus))
-            return status;
-        switch (commandStatus.getCondition()) {
-            case tempo_command::CommandCondition::kHelpRequested:
-                tempo_command::display_help_and_exit({"dump-lyric-object"},
-                    "Dump a lyric object into a human-readable format",
-                    {}, cmdGroupings, optMappings, argMappings, cmdDefaults);
-            default:
-                return status;
-        }
-    }
-
-    tempo_command::CommandConfig cmdConfig;
-
-    // convert options to config
-    TU_RETURN_IF_NOT_OK (convert_options(cmdOptions, optMappings, cmdConfig));
-
-    // convert arguments to config
-    TU_RETURN_IF_NOT_OK (convert_arguments(cmdArguments, argMappings, cmdConfig));
-
-    TU_LOG_INFO << "cmd config:\n" << tempo_command::command_config_to_string(cmdConfig);
+    TU_RETURN_IF_NOT_OK (command.parse(argc - 1, &argv[1]));
 
     // configure logging
     tempo_utils::LoggingConfiguration logging = {
@@ -86,16 +40,13 @@ dump_lyric_object(int argc, const char *argv[])
     };
 
     bool silent;
-    TU_RETURN_IF_NOT_OK(tempo_command::parse_command_config(silent, silentParser,
-        cmdConfig, "silent"));
+    TU_RETURN_IF_NOT_OK(command.convert(silent, silentParser, "silent"));
     if (silent) {
         logging.severityFilter = tempo_utils::SeverityFilter::kSilent;
     } else {
         int verbose, quiet;
-        TU_RETURN_IF_NOT_OK(tempo_command::parse_command_config(verbose, verboseParser,
-            cmdConfig, "verbose"));
-        TU_RETURN_IF_NOT_OK(tempo_command::parse_command_config(quiet, quietParser,
-            cmdConfig, "quiet"));
+        TU_RETURN_IF_NOT_OK(command.convert(verbose, verboseParser, "verbose"));
+        TU_RETURN_IF_NOT_OK(command.convert(quiet, quietParser, "quiet"));
         if (verbose && quiet)
             return tempo_command::CommandStatus::forCondition(tempo_command::CommandCondition::kCommandError,
                 "cannot specify both -v and -q");
@@ -116,8 +67,7 @@ dump_lyric_object(int argc, const char *argv[])
 
     // determine the object path
     std::filesystem::path objectPath;
-    TU_RETURN_IF_NOT_OK(tempo_command::parse_command_config(objectPath, objectPathParser,
-        cmdConfig, "objectPath"));
+    TU_RETURN_IF_NOT_OK(command.convert(objectPath, objectPathParser, "objectPath"));
 
     // read the lyric object file into memory
     tempo_utils::FileReader objectReader(objectPath);
